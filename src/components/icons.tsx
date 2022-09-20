814f71549dd2fc3762179a95fc1600207be3a4f2
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-09-20 13:44:03 -0400

    Add simplified icon imports

---
+++ src/components/icons.tsx
@@ -0,0 +1,25 @@
+export {
+  FaAws as Aws,
+  FaBootstrap as Bootstrap,
+  FaCss3Alt as Css,
+  FaGithub as Github,
+  FaGithubSquare as GithubSquare,
+  FaHtml5 as Html,
+  FaLinkedinIn as Linkedin,
+  FaLinkedin as LinkedinSquare,
+  FaNodeJs as Node,
+  FaReact as React,
+  FaSlack as Slack,
+  FaTwitter as Twitter,
+  FaTwitterSquare as TwitterSquare,
+} from 'react-icons/fa'
+
+export {
+  SiExpress as Express,
+  SiGraphql as Graphql,
+  SiJavascript as Javascript,
+  SiJest as Jest,
+  SiMongodb as MongoDb,
+  SiMysql as Mysql,
+  SiTypescript as Typescript,
+} from 'react-icons/si'
