984bfb6ab06e21f89f147791a63399ea5b8255f3
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-09-20 16:26:58 -0400

    Refactor for extraneous component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,21 @@
 import { Flex, Stack, Heading, Link, IconButton, useColorMode } from '@chakra-ui/react';
 import { LightMode, DarkMode } from './icons';
-import { Key } from 'react';
 
 export default function Header({ sections }: { sections: Array<string> }) {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Flex
+      as='header'
       justifyContent='space-between'
       w='100vw'
       py={1}
       px='10%'
     >
       <Heading as='h1'>Jason Fox</Heading>
-      <Flex justifyContent='space-evenly'>
-        <Stack as='nav' direction='row' alignItems='center'>
-          {sections.map((s: string, i: number) => <NavItem key={i} title={s} />)}
+      <Flex as='nav' justifyContent='space-evenly'>
+        <Stack as='ul' listStyleType='none' direction='row' alignItems='center'>
+          {sections.map((s: string, i: number) =>
+            <li key={i}><Link href={`#${s.toLowerCase().replaceAll(' ', '')}`}>{s}</Link></li>)}
         </Stack>
         <IconButton
           aria-label='Toggle theme'
@@ -23,11 +24,5 @@ export default function Header({ sections }: { sections: Array<string> }) {
         />
       </Flex>
     </Flex>
-  )
-}
-
-function NavItem({ title }: { key: Key, title: string }) {
-  return (
-    <div>{title}</div>
   )
 }
