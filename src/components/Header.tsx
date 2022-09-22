63775eba68bc943381d6934ec4113f721da6fcbd
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-09-21 21:24:35 -0400

    Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
-import { Flex, Stack, Heading, Button, IconButton, useColorMode } from '@chakra-ui/react';
+import { useRef } from 'react';
+import {
+  Flex, Stack, Show, Hide,
+  Heading, Button, IconButton,
+  Drawer, DrawerOverlay, DrawerContent,
+  useColorMode, useDisclosure,
+} from '@chakra-ui/react';
 import { LightMode, DarkMode } from './icons';
 import { scrollTo } from '../utils';
 
 export default function Header({ sections }: { sections: Array<string> }) {
   const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <Flex
       as='header'
@@ -11,6 +18,8 @@ export default function Header({ sections }: { sections: Array<string> }) {
       position='sticky'
       zIndex='9999'
       top='0'
+      h='10vh'
+      maxH='75px'
       w='100%'
       px='10%'
       py={4}
@@ -22,26 +31,37 @@ export default function Header({ sections }: { sections: Array<string> }) {
         justifyContent='space-evenly'
         spacing={8}
       >
-        <Stack
-          as='ul'
-          direction='row'
-          alignItems='center'
-          listStyleType='none'
-          spacing={{md: 4, lg: 6, xl: 8}}
-        >
-          {sections.map((s: string, i: number) => (
-            <li key={i}>
-              <Button
-                variant='nav'
-                onClick={() => scrollTo(s.toLowerCase().replaceAll(' ', ''))}
-              >
-                {s}
-              </Button>
-            </li>
-          ))}
-        </Stack>
+        <Show above='md'>
+          <Stack
+            as='ul'
+            direction='row'
+            alignItems='center'
+            listStyleType='none'
+            spacing={{ md: 4, lg: 6, xl: 8 }}
+          >
+            {sections.map((s: string, i: number) => (
+              <li key={i}>
+                <Button variant='nav' onClick={() => scrollTo(s.toLowerCase().replaceAll(' ', ''))}>{s}</Button>
+              </li>
+            ))}
+          </Stack>
+        </Show>
+        <Hide above='md'>
+          <Button variant='ghost' onClick={isOpen ? onClose : onOpen}>{isOpen ? 'X' : '='}</Button>
+          <Drawer
+            isOpen={isOpen}
+            placement='right'
+            onClose={onClose}
+          >
+            <DrawerOverlay />
+            {/* TODO: Use getBoundingRect observer whatever to get offset */}
+            <DrawerContent mt='10vh'>
+              sfsdkmsl
+            </DrawerContent>
+          </Drawer>
+        </Hide>
         <IconButton
-        variant='toggle'
+          variant='toggle'
           aria-label='Toggle theme'
           icon={colorMode === 'light' ? <DarkMode /> : <LightMode />}
           onClick={toggleColorMode}
