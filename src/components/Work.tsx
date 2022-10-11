a8e48eabf5d307ab1bd16283f39b71581f858e18
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-10-11 13:20:00 -0400

    Update projects

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,17 +1,17 @@
 import { Box, Stack, SimpleGrid } from "@chakra-ui/react";
 import { SectionTitle, FeaturedProjectCard, ProjectCard } from "./partials";
-import wtlUrl from '../assets/worlds-tier-list-project.png';
+import gbUrl from '../assets/gankbait-project.png';
 import cbUrl from '../assets/chopping-block-project.png';
 
 const featuredProjects = [
   {
-    imageUrl: wtlUrl,
+    imageUrl: gbUrl,
     mobile: false,
-    title: 'Worlds Tier List Maker',
-    tags: ['React', 'React Beautiful DnD', 'CSS'],
-    description: 'Clean and simple tier list maker for League of Legends Worlds 2022 teams with filters and rendered image/text output.',
-    liveUrl: 'https://www.worlds.jthefox.com/',
-    sourceUrl: '',
+    title: 'Gankbait',
+    tags: ['Serverless', 'Pug', 'OAuth2', 'MongoDB'],
+    description: 'GCP-hosted SPA for analyzing match timeline data from the Riot match-v5 API.',
+    liveUrl: 'https://gankbait-36ce7.web.app/',
+    sourceUrl: 'https://github.com/JtheFox/gankbait',
   },
   {
     imageUrl: cbUrl,
@@ -26,23 +26,23 @@ const featuredProjects = [
 
 const projects = [
   {
-    title: 'Portfolio',
-    tags: ['TypeScript', 'React', 'Chakra UI'],
-    description: 'My portfolio. You\'re already here!',
+    title: 'OneStats',
+    tags: ['Electron', 'React', 'Material UI'],
+    description: 'React-based Electron application scaffolded with Vite designed to fetch and parse bulk match data from Riot API.',
     liveUrl: '',
-    sourceUrl: 'https://github.com/jthefox/portfolio',
+    sourceUrl: 'https://github.com/jthefox/OneStats',
   },
   {
-    title: 'Gankbait',
-    tags: ['Express', 'Pug', 'Bulma', 'OAuth2', 'MongoDB'],
-    description: 'SPA for analyzing match timeline data from the Riot League of Legends match API.',
-    liveUrl: 'https://gankbait-36ce7.web.app/',
-    sourceUrl: 'https://github.com/JtheFox/gankbait',
+    title: 'Tierlisted.gg',
+    tags: ['React', 'React Beautiful DnD', 'CSS'],
+    description: 'Clean and simple tier list maker for League of Legends Worlds 2022 teams with filters and rendered image/text output.',
+    liveUrl: 'https://tierlisted.gg',
+    sourceUrl: '',
   },
   {
     title: 'Rosiebot',
     tags: ['Discordjs', 'Sequelize', 'Bootstrap', 'AWS'],
-    description: 'Your friendly neighborhood Discord c(h)atbot.',
+    description: 'Your friendly neighborhood Discord c(h)atbot. Hosted on AWS.',
     liveUrl: 'https://jthefox.github.io/rosiebot/',
     sourceUrl: 'https://github.com/JtheFox/rosiebot',
   },
@@ -56,7 +56,7 @@ const projects = [
   {
     title: 'Password Generator',
     tags: ['HTML', 'CSS', 'JavaScript'],
-    description: 'Generate a secure password with the click of a button.',
+    description: 'Simple SPA to generate a secure password with the click of a button.',
     liveUrl: 'https://jthefox.github.io/password-generator/',
     sourceUrl: 'https://github.com/JtheFox/password-generator',
   },
